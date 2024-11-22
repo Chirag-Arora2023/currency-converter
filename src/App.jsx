bfe2ea9c4179170358b8760991eeb7f85a7aaaa1
Chirag Arora <cse.chirag.arora@gmail.com>
2024-11-22 10:58:52 +0530

    minor bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -27,16 +27,17 @@ function App() {
      <div
         className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
         style={{
-           backgroundImage: `url(https://images.pexels.com/photos/4497591/pexels-photo-4497591.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2)`,
+           backgroundImage: `url(https://images.pexels.com/photos/210607/pexels-photo-210607.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2)`,
         }}
      >
         <div className="w-full">
            <div className="w-full max-w-lg md:text-xl mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
               <form
-              onSubmit={(e)=>{
-               e.preventDefault()
-               convert()
-               }}>
+                 onSubmit={(e) => {
+                    e.preventDefault();
+                    convert();
+                 }}
+              >
                  <div className="w-full mb-1">
                     <InputBox
                        currency={from}
@@ -49,9 +50,10 @@ function App() {
                     />
                  </div>
                  <div className="relative w-full h-0.5">
-                    <button 
-                     className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
-                     onClick={swap}>
+                    <button
+                       className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                       onClick={swap}
+                    >
                        Swap
                     </button>
                  </div>
